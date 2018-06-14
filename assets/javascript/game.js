cde73c3f28c9b288d34e87403652f80ac5e9f9ae
josef utleg <josefutleg@gmail.com>
2018-06-13 23:25:56 -0700

    added more songs to songList array. added top margin to body

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
     // create an array of songs
-    var songList = ['nikes', 'pyramids','ivy','lens','nights','chanel','provider','biking', 'white','novacane','dust','solo','godspeed','voodoo'];    
+    var songList = ['nikes', 'pyramids','ivy','lens','nights','chanel','provider','biking', 'white','novacane','dust','solo','godspeed','voodoo','lost','monks','siegfried','lovecrimes'];    
     // choose a random song from array
     var randomSong;
     // create array to store correct letters from user
@@ -39,7 +39,7 @@
             document.querySelector('h2').innerText = '2 more!';
         }
         if (wins === 3){
-            document.querySelector('h2').innerText = 'almost there!';
+            document.querySelector('h2').innerText = '1 more!';
         }
         if (wins === 4){
             document.querySelector('.container').innerHTML = playVideo;
@@ -82,18 +82,18 @@
 
         if (randomSong === userGuess){
             correctLetter = [];
-            guessesCounter = 15;
+            // guessesCounter = 15;
             wins = wins + 1;
             wrongLetter = [];
             surpriseVideo();
             document.getElementById('guessesCounter').innerText = guessesCounter;
             document.getElementById('userGuesses').innerText = "great! play again!";
-            return;   
+            return;  
         }
         if (guessesCounter === 0){
             correctLetter = [];
             wins = 0;
-            guessesCounter = 15;
+            // guessesCounter = 15;
             wrongLetter = [];
             document.querySelector('h2').innerText = "name frank's song!";
             document.getElementById('guessesCounter').innerText = guessesCounter;
