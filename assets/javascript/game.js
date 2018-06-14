1b0de0dfba085e13fbe5a6328440d850ed4f2f95
josef utleg <josefutleg@gmail.com>
2018-06-13 22:29:39 -0700

    added a new function that plays a video after 4 consecutive wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,20 @@
     var randomSong;
     // create array to store correct letters from user
     var correctLetter = [];
-    // create array to store wrong letters from user
+    var wins = 0;
     var wrongLetter = [];
     // create an array that will change random song into an under score
-    // var underScore = [];
+    var playVideo = '<iframe width="500" height="500" src="https://www.youtube.com/embed/jgNjn2b1Jl0?rel=0&autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
     var guessesCounter = 15;
     var docUserGuesses = 'press any letter to start!';
     var s;
     
     document.getElementById('guessesCounter').innerText = guessesCounter;
     document.getElementById('userGuesses').innerText = docUserGuesses;
+    // document.querySelector('.container').innerHTML = playVideo;
+
+    
+
 
     function startUp(){
         randomSong = songList[Math.floor(Math.random() * songList.length)];
@@ -25,6 +29,22 @@
         // debugger;
     }
     startUp();
+
+    function surpriseVideo (){
+        if (wins === 1){
+            document.querySelector('h2').innerText = 'win 3 more for a surprise!';
+            return;
+        }
+        if (wins === 2){
+            document.querySelector('h2').innerText = '2 more!';
+        }
+        if (wins === 3){
+            document.querySelector('h2').innerText = 'almost there!';
+        }
+        if (wins === 4){
+            document.querySelector('.container').innerHTML = playVideo;
+        }
+    }
 
     console.log(randomSong);
 
@@ -40,7 +60,6 @@
 
     function play(x){
         var userInput = x.key;
-
 
         if(userInput !== randomSong) 
         {
@@ -58,26 +77,31 @@
                 return correctLetter[id]
             });
             document.querySelector("#letter").innerText = correctLetter.join(` `);
-
         }
         userGuess = correctLetter.join('').toString();
 
         if (randomSong === userGuess){
+            correctLetter = [];
             guessesCounter = 15;
+            wins = wins + 1;
             wrongLetter = [];
+            surpriseVideo();
             document.getElementById('guessesCounter').innerText = guessesCounter;
             document.getElementById('userGuesses').innerText = "great! play again!";
+            return;   
         }
         if (guessesCounter === 0){
             correctLetter = [];
+            wins = 0;
             guessesCounter = 15;
             wrongLetter = [];
+            document.querySelector('h2').innerText = "name frank's song!";
             document.getElementById('guessesCounter').innerText = guessesCounter;
             document.getElementById('userGuesses').innerText = `:( it was ${randomSong}, play again!`;
             return;
         }
-}   
 
+}   
 
 
 document.onkeyup = play;
